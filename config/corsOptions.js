63b93a472b21a1714921b0f4c65acaf4158184c2
Hussein Basil <husseingarnawi@gmail.com>
2023-07-31 16:45:27 +0300

    Config uncommented

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,13 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    // origin: (origin, callback) => {
-    //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-    //         callback(null, true)
-    //     } else {
-    //         callback(new Error('Not allowed by CORS'));
-    //     }
-    // },
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     optionsSuccessStatus: 200,
 }
 
